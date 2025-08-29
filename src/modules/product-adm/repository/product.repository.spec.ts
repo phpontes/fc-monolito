91fe21b6e6b9222c36f11484160510599d23769e
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-08-29 19:29:44 +0000

    implementando teste create product

--- src/modules/product-adm/repository/product.repository.spec.ts
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -1,5 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "./product.model";
+import Product from "../domain/product.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import ProductRepository from "./product.repository";
 
 describe("ProductRepository test", () => {
   let sequelize: Sequelize;
@@ -20,5 +23,26 @@ describe("ProductRepository test", () => {
     await sequelize.close();
   });
 
-  it("should create a product", async () => {});
+  it("should create a product", async () => {
+    const productProps = {
+      id: new Id("1"),
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    };
+    const product = new Product(productProps);
+    const productRepository = new ProductRepository();
+    await productRepository.add(product);
+
+    const productDb = await ProductModel.findOne({
+      where: { id: productProps.id.id },
+    });
+
+    expect(productProps.id.id).toEqual(productDb.id);
+    expect(productProps.name).toEqual(productDb.name);
+    expect(productProps.description).toEqual(productDb.description);
+    expect(productProps.purchasePrice).toEqual(productDb.purchasePrice);
+    expect(productProps.stock).toEqual(productDb.stock);
+  });
 });
