82f85a02e9dddd50cba302ef9ea0366a7ff19531
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-08-29 19:48:39 +0000

    implementando teste find a product
    refactor: update ProductModel to use declare syntax - compatible with newer sequelize

--- src/modules/product-adm/repository/product.repository.spec.ts
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
-import { ProductModel } from "./product.model";
-import Product from "../domain/product.entity";
 import Id from "../../@shared/domain/value-object/id.value-object";
+import Product from "../domain/product.entity";
+import { ProductModel } from "./product.model";
 import ProductRepository from "./product.repository";
 
 describe("ProductRepository test", () => {
@@ -44,5 +44,27 @@ describe("ProductRepository test", () => {
     expect(productProps.description).toEqual(productDb.description);
     expect(productProps.purchasePrice).toEqual(productDb.purchasePrice);
     expect(productProps.stock).toEqual(productDb.stock);
+  });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+
+    ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const product = await productRepository.find("1");
+
+    expect(product.id.id).toEqual("1");
+    expect(product.name).toEqual("Product 1");
+    expect(product.description).toEqual("Product 1 description");
+    expect(product.purchasePrice).toEqual(100);
+    expect(product.stock).toEqual(10);
   });
 });
